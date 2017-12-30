ac61c5a0d5283226bf59e839abf637df3570e841
galledohm <adrigalle.san@gmail.com>
2017-12-30 20:53:44 +0100

    Reverb e imagen ilustrativa añadidas.

---
+++ Efectos/Reverb/reverb.d
@@ -0,0 +1,7 @@
+.\reverb.o: reverb.c
+.\reverb.o: C:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.H
+.\reverb.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\reverb.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\reverb.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\reverb.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\reverb.o: C:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
